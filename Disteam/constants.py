1b0a86d28861a31b03b49da94436a28ff57db2f3
DevRuby <60344644+dev-ruby@users.noreply.github.com>
2023-03-13 22:47:22 +0900

    Added getUserLevel, getUserOwnedGameCount Method

--- Disteam/constants.py
+++ Disteam/constants.py
@@ -12,3 +12,5 @@ class URLS:
     RECENT_GAME = "https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/"
     USER_INFO = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/"
     CUSTOM_URL = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
+    USER_LEVEL = "https://api.steampowered.com/IPlayerService/GetSteamLevel/v1/"
+    OWNED_GAMES = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/"
